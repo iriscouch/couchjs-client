00d5bfb53abcd62f02f5a00b500bd0b6022b14a0
Jason Smith (work) <jhs@iriscouch.com>
2013-01-30 15:58:45 +0000

    A function to run through a log file and parse it

--- cli.js
+++ cli.js
@@ -26,6 +26,7 @@ var LineStream = require('couchjs/stream')
 
 
 var opts = optimist.usage('$0 </path/to/couchjs> </path/to/main.js>')
+                   .describe('confirm=<path>', 'Path to a log file to test')
 
 
 function toSource() {
@@ -41,6 +42,9 @@ function toSource() {
 function main() {
   var couchjs_filename = opts.argv._[0]
     , main_js = opts.argv._[1]
+
+  if(opts.argv.help)
+    return console.log(opts.help())
 
   if(!main_js || !couchjs_filename)
     return console.error(opts.help())
@@ -67,9 +71,54 @@ function main() {
     console.log('couchjs %s: %s', child.pid, line)
   }
 
-  setTimeout(function() {
-    child.stdin.write('[]\n')
-  }, 3000)
+  if(opts.argv.confirm)
+    confirm_log(child, opts.argv.confirm, function(er) {
+      if(er)
+        throw er
+    })
+    return confirm_log(child, opts.argv.confirm)
+
+  console.log('Done')
+  child.stdin.write('[]\n')
+}
+
+function confirm_log(couchjs, log_filename, callback) {
+  var log = new LineStream
+  log.on('data', log_line)
+  log.on('end', log_end)
+
+  var log_data = fs.createReadStream(log_filename)
+  log_data.setEncoding('utf8')
+
+  var expect = 'STDIN'
+    , io_re = /^(STDIN|STDOUT) (\d+): (.*)$/
+
+  log_data.pipe(log)
+
+  function log_line(line) {
+    var match = line.match(io_re)
+    if(!match)
+      return
+
+    var direction = match[1]
+      , message = JSON.parse(match[3])
+
+    if(direction !== expect) {
+      couchjs.kill()
+      return callback(new Error('Expected direction '+expect+' but got ' + JSON.stringify(direction) + ': ' + line))
+    }
+
+    console.log('Log %s: %j', direction, message)
+
+    expect = (direction === 'STDIN')
+                ? 'STDOUT'
+                : 'STDIN'
+  }
+
+  function log_end() {
+    console.log('End of log')
+    couchjs.kill()
+  }
 }
 
 if(require.main === module)
